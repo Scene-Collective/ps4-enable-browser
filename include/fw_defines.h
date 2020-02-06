f7d95fdcf99506399568316c619f8f8b61557786
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2020-02-06 14:56:05 -0800

    Add 5.03 Support

--- include/fw_defines.h
+++ include/fw_defines.h
@@ -19,21 +19,21 @@
 #define K455_ROOTVNODE             0x021AFA30
 #define K474_ROOTVNODE             0x021B89E0
 #define K501_ROOTVNODE             0x022C19F0
-#define K503_ROOTVNODE             0x0
+#define K503_ROOTVNODE             0x022C1A70
 #define K505_ROOTVNODE             0x022C1A70
 
 #define K405_PRINTF                0x00347580
 #define K455_PRINTF                0x00017F30
 #define K474_PRINTF                0x00017F30
 #define K501_PRINTF                0x00435C70
-#define K503_PRINTF                0x0
+#define K503_PRINTF                0x00436000
 #define K505_PRINTF                0x00436040
 
 #define K405_COPYIN                0x00286DF0
 #define K455_COPYIN                0x0014A890
 #define K474_COPYIN                0x00149F20
 #define K501_COPYIN                0x001EA600
-#define K503_COPYIN                0x0
+#define K503_COPYIN                0x001EA710
 #define K505_COPYIN                0x001EA710
 
 #define K405_COPYOUT               0x00286D70
@@ -47,7 +47,7 @@
 #define K455_REG_MGR_SET_INT       0x004D6F00
 #define K474_REG_MGR_SET_INT       0x004D8580
 #define K501_REG_MGR_SET_INT       0x004F8940
-#define K503_REG_MGR_SET_INT       0x0
+#define K503_REG_MGR_SET_INT       0x004F8CD0
 #define K505_REG_MGR_SET_INT       0x004F8D10
 
 #endif
